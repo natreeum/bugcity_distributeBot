5f6a29f8b0f15bd8f381bdeb1454c4f309b675f1
natreeum <projectkmscom@gmail.com>
2022-11-01 19:46:56 +0900

    fix: balance check

--- functions/distributeFunction.js
+++ functions/distributeFunction.js
@@ -80,6 +80,7 @@ async function distribute(interaction) {
       }
       //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
       const balance = await bankManager.getBalance(customer.id);
+      console.log(balance);
       if (balance < wageSum) {
         return await interaction.reply(
           `<@${customer.id}> 사장님 잔고에 충분한 급여가 존재하지 않습니다.\nDAO4DAO에서 급여를 분배받았는지 확인해 주세요.`
